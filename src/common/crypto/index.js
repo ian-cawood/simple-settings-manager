8700ab0560595e1042a9a52dbf81b56f3841999c
ian-cawood <ian.cawood@gmail.com>
2020-06-20 10:01:18 +0200

    feat: add encrypt and decrypt utility functions

--- src/common/crypto/index.js
+++ src/common/crypto/index.js
@@ -7,3 +7,29 @@ const generateECCKeys = (userIds, passphrase = '') => openpgp.generateKey({
 })
 
 module.exports.generateECCKeys = generateECCKeys
+
+const encryptString = async (publicKeyArmored, message) => {
+  const publicKeys = (await openpgp.key.readArmored(publicKeyArmored)).keys
+
+  const { data: encrypted } = await openpgp.encrypt({
+    message: openpgp.message.fromText(message), 
+    publicKeys,
+  })
+
+  return encrypted
+}
+
+module.exports.encryptString = encryptString
+
+const decryptString = async (privateKeyArmored, cipherText) => {
+  const privateKey = (await openpgp.key.readArmored([privateKeyArmored])).keys[0]
+
+  const { data: decrypted } = await openpgp.decrypt({
+    message: await openpgp.message.readArmored(cipherText),
+    privateKeys: [privateKey]
+  })
+
+  return decrypted
+}
+
+module.exports.decryptString = decryptString
--- src/common/crypto/index.test.js
+++ src/common/crypto/index.test.js
@@ -1,4 +1,5 @@
 const assert = require('chai').assert
+const openpgp = require('openpgp')
 
 const SUT = require('./index')
 
@@ -18,6 +19,48 @@ describe('common/crypto', function () {
       const result = await SUT.generateECCKeys([{ email: 'example@test.com' }], 'super secure passphrase')
 
       assert.hasAllKeys(result, ['key', 'privateKeyArmored', 'publicKeyArmored', 'revocationCertificate'])
+    })
+  })
+
+  describe('encryptString', function () {
+    it('should be a function', function () {
+      assert.isFunction(SUT.encryptString, 'should be a function')
+    })
+
+    it('should encrypt a string with given public key', async function () {
+      const { publicKeyArmored } = await openpgp.generateKey({
+        userIds: [{ email: 'example@test.com' }],
+        curve: 'ed25519',
+      })
+   
+      const encryptedString = await SUT.encryptString(publicKeyArmored, 'hello')
+
+      assert.isString(encryptedString, 'the data should still be a string')
+    })
+  })
+
+  describe('decryptString', function () {
+    it('should be a function', function () {
+      assert.isFunction(SUT.decryptString, 'should be a function')
+    })
+
+    it('should decrypt a string with given private key', async function () {
+      const { publicKeyArmored, privateKeyArmored } = await openpgp.generateKey({
+        userIds: [{ email: 'example@test.com' }],
+        curve: 'ed25519',
+      })
+   
+      const publicKeys = (await openpgp.key.readArmored(publicKeyArmored)).keys
+
+      const { data: encrypted } = await openpgp.encrypt({
+        message: openpgp.message.fromText('hello'),
+        publicKeys,
+      })
+
+      const decryptedString = await SUT.decryptString(privateKeyArmored, encrypted)
+
+      assert.isString(decryptedString, 'the data should still be a string')
+      assert.strictEqual(decryptedString, 'hello')
     })
   })
 })
