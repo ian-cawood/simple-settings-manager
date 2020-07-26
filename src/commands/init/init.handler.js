2937b9059457edf8bc6756227d675ac50331ad84
ian-cawood <ian.cawood@gmail.com>
2020-07-26 18:58:37 +0200

    chore: add basic save public key functionality

--- src/commands/init/init.handler.js
+++ src/commands/init/init.handler.js
@@ -3,6 +3,7 @@ const inquirer = require('inquirer')
 const promts = require('./promts')
 const file = require('../../common/file')
 const json = require('../../common/json')
+const crypto = require('../../common/crypto')
 const { defaultConfigFile, defaultFileStructure } = require('./constants')
 
 const handler = () => {
@@ -11,16 +12,19 @@ const handler = () => {
       promts.baseFileName,
       promts.stages,
     ])
-    .then(answers => {
+    .then(async answers => {
       const { stages, baseFileName } = answers
       stages.forEach(stage => 
         file.write(`${baseFileName}.${stage}.json`, json.stringifyWith2Spaces(defaultFileStructure))
       )
+      const { privateKeyArmored, publicKeyArmored } = await crypto.generateECCKeys([{ email: 'test@test.com'}], '')
+      console.log(privateKeyArmored)
       const configFile = {
         ...defaultConfigFile,
         stages,
         baseFileName,
-        fileType: 'json'
+        fileType: 'json',
+        publicKeyArmored,
       }
       file.write('ssm.config.json', json.stringifyWith2Spaces(configFile))
     })
