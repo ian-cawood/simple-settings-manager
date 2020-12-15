859ab0f024f13f76be2e47437e4ca5a6060e140c
ian-cawood <ian.cawood@gmail.com>
2020-12-15 16:16:18 +0200

    chore: add read util

--- src/common/file/index.js
+++ src/common/file/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 
 const write = (fileName, content) => fs.writeFileSync(fileName, content)
-
 module.exports.write = write
+
+const read = (fileName) => fs.readFileSync(fileName, 'utf8')
+module.exports.read = read
